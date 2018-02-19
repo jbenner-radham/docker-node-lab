0319d289e4f2f19e7926ff9fa6eee95938751423
James Benner <james.benner@gmail.com>
2018-02-18 20:53:06 -0500

    Linked up the two containers.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const Bq = require('@becquerel/framework');
 const mongoose = require('mongoose');
 
-console.log('Hello world!', typeof mongoose);
+const app = new Bq();
+const host = (process.env['NODE_ENV'] === 'production')
+    ? 'db'
+    : 'localhost';
+
+mongoose.connect(`mongodb://${host}/stuff`);
+
+const Cat = mongoose.model('Cat', {name: String});
+const kitty = new Cat({name: 'Evil'});
+
+kitty.save().then(() => console.log('Yipee!'))
+    .catch(() => console.error('Boo!'))
+    .then(() => mongoose.disconnect());
+
+process.on('SIGINT', () => {
+    mongoose.disconnect().then(() => {
+        console.log('Mongo disconnected.');
+        process.exit(0);
+    });
+});
