5ea25ca467ae18aacd0242b0ff62f122d8559172
James Benner <james.benner@gmail.com>
2018-02-23 09:43:05 -0500

    Code cleanup.

--- src/index.js
+++ src/index.js
@@ -5,19 +5,10 @@ const mongoose = require('mongoose');
 
 const app = new Bq();
 const host = (process.env['NODE_ENV'] === 'production')? 'db' : 'localhost';
+const url = `mongodb://${host}/stuff`;
 const getReadyStateName = (connection) => connection.states[connection.readyState];
 
-console.log(
-    mongoose.connection.readyState,
-    getReadyStateName(mongoose.connection)
-);
-
-mongoose.connect(`mongodb://${host}/stuff`);
-
-console.log(
-    mongoose.connection.readyState,
-    getReadyStateName(mongoose.connection)
-);
+mongoose.connect(url);
 
 const Cat = mongoose.model('Cat', new mongoose.Schema({name: String}));
 const kitty = new Cat({name: 'Evil'});
