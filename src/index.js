f0081ec46b6b0ee77a2f1e525fcb51dbbc49aba4
James Benner <james.benner@gmail.com>
2018-02-23 09:27:48 -0500

    Playing around a bit more.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ const Bq = require('@becquerel/framework');
 const mongoose = require('mongoose');
 
 const app = new Bq();
-const host = (process.env['NODE_ENV'] === 'production')
-    ? 'db'
-    : 'localhost';
+const host = (process.env['NODE_ENV'] === 'production')? 'db' : 'localhost';
+const getReadyStateName = (connection) => connection.states[connection.readyState];
+
+console.log(
+    mongoose.connection.readyState,
+    getReadyStateName(mongoose.connection)
+);
 
 mongoose.connect(`mongodb://${host}/stuff`);
 
-const Cat = mongoose.model('Cat', {name: String});
+console.log(
+    mongoose.connection.readyState,
+    getReadyStateName(mongoose.connection)
+);
+
+const Cat = mongoose.model('Cat', new mongoose.Schema({name: String}));
 const kitty = new Cat({name: 'Evil'});
 
 kitty.save().then(() => console.log('Yipee!'))
@@ -21,6 +30,13 @@ kitty.save().then(() => console.log('Yipee!'))
             .catch(() => console.error(`Couldn't find the cat :(`))
             .then(() => mongoose.disconnect());
     });
+
+/*
+Cat.find({name: 'Evil?'})
+    .then(results => console.log(results))
+    .catch(error => console.error(error))
+    .then(() => mongoose.disconnect())
+*/
 
 process.on('SIGINT', () => {
     mongoose.disconnect().then(() => {
